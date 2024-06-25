5400ce4a6b631775d3330f460822ed65129c2467
Brian Kimathi M <briankimathi94@gmail.com>
2024-06-25 13:38:58 +0300

    hello

--- src/REALESTATE_backend/src/main.py
+++ src/REALESTATE_backend/src/main.py
@@ -1,5 +1,13 @@
-from kybra import query
+from kybra import query, update
+
 
 @query
 def greet(name: str) -> str:
     return f"Hello, {name}!"
+
+# update
+
+
+@update
+def increment(n: int) -> int:
+    return n + 1
